1ebabae58bcc7a685c7ba11d1932507dffcf2b1e
AgnesEkblom <agnes.ekblom@gmail.com>
2019-02-02 21:15:35 +0100

    Projektet(tm)

---
+++ Klunka eller Konsekvens/Assets/Scripts/Main.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    public StartGui SG;
+    public Text texst;
+    public string[] CasualKlunka;
+    public string[] IntenseKlunka;
+    public string[] CasualKonsekvens;
+    public string[] IntenseKonsekvens;
+
+    public int randomCasualKlunka;
+    public void Awake() {
+        SG = GetComponent<StartGui>();
+    }
+
+    void Update()
+    {
+        if (SG.intensity == 1)
+        {
+            Casual();
+            
+        }
+        if (SG.intensity == 2)
+        {
+            Intense();
+        }
+
+    }
+
+
+
+    public void Casual()
+    {
+        if (SG.KlunkaKonsekvens == 1)
+        {
+           randomCasualKlunka = Random.Range(0, CasualKlunka.Length);
+            string currentText;
+            texst.text = randomCasualKlunka.ToString();
+            print(texst);
+            SG.KlunkaKonsekvens = 0;
+        }
+        if (SG.KlunkaKonsekvens == 2)
+        {
+            print("CasualKonsekvens");
+        }
+
+
+    }
+    public void Intense()
+    {
+        if (SG.KlunkaKonsekvens == 1)
+        {
+
+        }
+        if (SG.KlunkaKonsekvens == 2)
+        {
+
+        }
+    }
+
+}
---
+++ Klunka eller Konsekvens/Assets/Scripts/StartGui.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGui : MonoBehaviour
+{
+    public int XButtonLocation;
+    public int YLocation1;
+    public int YLocation2;
+    public Texture btnTexture;
+    public bool Guiswitch;
+    public int intensity=0;
+    public int KlunkaKonsekvens;
+    
+    
+
+    private void Start() {
+        XButtonLocation = Screen.width/2;
+        Guiswitch = false;
+    }
+    void OnGUI()
+    {
+
+        if (Guiswitch == false)
+        {
+
+            if (GUI.Button(new Rect(XButtonLocation, YLocation1, 100, 75), "Casual"))
+            {
+                Guiswitch = true;
+                intensity = 1;
+
+
+            }
+            if (GUI.Button(new Rect(XButtonLocation, YLocation2, 100, 75), "Intense"))
+            {
+                Guiswitch = true;
+                intensity = 2;
+
+            }
+
+
+        }
+
+
+
+        if (Guiswitch == true)
+        {
+            if (GUI.Button(new Rect(0, 0, 50, 30), "Return"))
+            {
+                Guiswitch = false;
+                intensity = 0;
+                KlunkaKonsekvens=0;
+            }
+
+
+                if (GUI.Button(new Rect(0, YLocation1, 100, 75), "Klunka"))
+                {
+                KlunkaKonsekvens = 1;
+                }
+                if (GUI.Button(new Rect(0, YLocation2, 100, 75), "Konsekvens"))
+                {
+                KlunkaKonsekvens = 2;
+                }
+           
+        }
+    }
+}
